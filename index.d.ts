6dabe6c97bd01a962e7a7883029637cea71d543a
Rohit Gohri <rohit.gohri@smartprix.com>
2019-04-03 14:04:44 +0530

    Add initial class and methods

---
+++ index.d.ts
@@ -0,0 +1,116 @@
+declare module '@smpx/slack' {
+	/**
+	 * Taken from @slack/client
+	 */
+	interface MessageAttachment {
+		fallback?: string;
+		color?: 'good' | 'warning' | 'danger' | string;
+		pretext?: string;
+		author_name?: string;
+		author_link?: string;
+		author_icon?: string;
+		title?: string;
+		title_link?: string;
+		text?: string;
+		fields?: {
+			title: string;
+			value: string;
+			short?: boolean;
+		}[];
+		image_url?: string;
+		thumb_url?: string;
+		footer?: string;
+		footer_icon?: string;
+		ts?: string;
+		actions?: AttachmentAction[];
+		callback_id?: string;
+		mrkdwn_in?: ('pretext' | 'text' | 'fields')[];
+	}
+	interface AttachmentAction {
+		id?: string;
+		confirm?: Confirmation;
+		data_source?: string;
+		min_query_length?: number;
+		name?: string;
+		options?: OptionField[];
+		option_groups?: {
+			text: string;
+			options: OptionField[];
+		}[];
+		selected_options?: OptionField[];
+		style?: string;
+		text: string;
+		type: string;
+		value?: string;
+		url?: string;
+	}
+	interface OptionField {
+		description?: string;
+		text: string;
+		value: string;
+	}
+	interface Confirmation {
+		dismiss_text?: string;
+		ok_text?: string;
+		text: string;
+		title?: string;
+	}
+
+	class Slack {
+		static logger: Console;
+		/**
+		 * Overwrite this function to skip slack message sending in some cnditions
+		 * and log the message instead. By default skips in test environment
+		 */
+		static logCondition: () => boolean;
+		/**
+		 * Username to send message with, default: 'slackbot'
+		 */
+		static username: string;
+
+		constructor(init?: {text?: string, channel?: string})
+		text(text: string): this;
+		channel(channelName: string): this;
+		attachment(attachments: MessageAttachment | MessageAttachment[]): this;
+		error(err: Error, opts?: {label?: string}): this;
+		/**
+		 * @param linkOrEmoji Url of image to use as icon or string for emoji
+		 */
+		icon(linkOrEmail: string): this;
+		send(): Promise<void>;
+
+		postMessage(text: string, opts?: {
+			channel?: string;
+			attachments?: MessageAttachment[],
+			extraProps?: object,
+		}): Promise<void>;
+
+		/**
+		 * Format function for message building, default formatting is Bold
+		 * @param text 
+		 * @param opts If no options object is provided then bold is set as true
+		 * @returns formatted txt
+		 */
+		static format(text: string, opts?: {
+			code?: boolean;
+			bold?: boolean;
+			italics?: boolean;
+			strikethrough?: boolean;
+		}): string;
+
+		static formatUrl(url: string, text: string): string;
+		/**
+		 * Webhook is given priority over legacy tokens
+		 * @param webhook
+		 */
+		static setWebhook(webhook: string);
+		static setToken(token: string);
+	}
+
+	export default Slack;
+
+	export {
+		MessageAttachment,
+		Slack,
+	}
+}
---
+++ index.js
@@ -0,0 +1,271 @@
+const {cfg, Connect} = require('sm-utils');
+const os = require('os');
+
+/**
+ * @typedef {import('@smpx/slack').MessageAttachment} attach
+ */
+
+let packageObj;
+function getPackageInfo() {
+	if (packageObj) return packageObj;
+	try {
+		packageObj = require(`${process.cwd()}/package.json`);
+	}
+	catch(err) {
+		packageObj = {name: 'Unknown'};
+	}
+	return packageObj;
+}
+
+function getSlackConf() {
+	return cfg('slack', {
+		token: '',
+		webhook: '',
+		channel: '',
+	});
+}
+
+/**
+ * @returns {attach}
+ */
+function getDefaultAttachment() {
+	const {name, version} = getPackageInfo();
+	const fields = [
+		{
+			title: 'Hostname:',
+			value: os.hostname(),
+			short: true,
+		},
+		{
+			title: 'App: ',
+			value: `${name} v${version}`,
+			short: true,
+		},
+		{
+			title: 'Node Environment',
+			value: cfg.getEnv(),
+			short: true,
+		},
+	];
+
+	if (process.env.name || process.env.pm_id) {
+		fields.push({
+			title: 'PM2 App',
+			value: `${process.env.name} ${process.env.pm_id || -1}`,
+			short: true,
+		});
+	}
+
+	return {
+		fields,
+	};
+}
+
+class Slack {
+	static get logger() {
+		return console;
+	} 
+	/**
+	 * Overwrite this function to skip slack message sending in some cnditions
+	 * and log the message instead. By default skips in test environment
+	 */
+	static logCondition() {
+		return cfg.isTest();
+	}
+	/**
+	 * Username to send message with, default: 'slackbot'
+	 */
+	static get username() {
+		return 'slackbot';
+	}
+
+	constructor({text = '', channel = ''} = {}) {
+		/** @type {attach[]} */
+		this._attachments = [];
+		this._errors = [];
+		this._extraProps = {};
+
+		if (channel) this.channel(channel);
+		if (text) this.text(text);
+	}
+
+	/** @param {string} text */
+	text(text) {
+		this._text = text;
+		return this;
+	}
+
+	channel(channelName) {
+		this._channel = channelName;
+		return this;
+	}
+
+	/**
+	 * @param {attach | attach[]} attachments
+	 */
+	attachment(attachments) {
+		if (!Array.isArray(attachments)) attachments = [attachments];
+		this._attachments.concat(attachments);
+		return this;
+	}
+
+	/**
+	 * @param {Error} err
+	 * @param {{label?: string}} [param1={}]
+	 */
+	error(err, {label = ''} = {}) {
+		this._errors.push(err);
+		const {bugs} = getPackageInfo();
+		const bugsUrl = bugs && bugs.url;
+
+		/** @type {attach} */
+		const attachment = {
+			pretext: `${Slack.format('Error')}: ${err.message}`,
+			text: err.stack,
+			color: 'danger',
+		};
+
+		if (bugsUrl) {
+			label = label ? `[${label}] ` : '';
+			attachment.actions = [{
+				type: 'button',
+				text: 'Create an issue for this error?',
+				url: `${bugsUrl}/new?title=${
+					encodeURIComponent(`${label}${err.message}`)
+				}&body=${
+					encodeURIComponent(`Error was: ${err.stack}`)
+				}&labels=bug`,
+			}];
+		}
+
+		return this.attachment(attachment);
+	}
+
+	/**
+	 * @param {string} linkOrEmoji Url of image to use as icon or string for emoji
+	 */
+	icon(linkOrEmoji) {
+		if (linkOrEmoji[0] === ':') {
+			this._extraProps.icon_emoji = linkOrEmoji;
+		}
+		else this._extraProps.icon_url = linkOrEmoji;
+		return this;
+	}
+
+	async send() {
+		return Slack
+			.postMessage(this._text, {
+				channel: this._channel,
+				attachments: this._attachments,
+				extraProps: this._extraProps,
+			});
+	}
+
+	/**
+	 * @private
+	 * @param {object} message 
+	 */
+	static async _postWithToken(message) {
+		message.token = getSlackConf().token;
+		try {
+			const connect = Connect.url('https://slack.com/api/chat.postMessage');
+			const res = await connect.fields(message).post();
+			const parsedBody = JSON.parse(res.body);
+			if (parsedBody.error) {
+				this.logger.error({label: 'Slack'}, message, parsedBody);
+			}
+		}
+		catch (err) {
+			this.logger.error({label: 'Slack'}, message, err);
+		}
+	}
+
+	static async _postWithWebhook(message) {
+		try {
+			const connect = Connect.url(getSlackConf().webhook);
+			const res = await connect.fields(message).post();
+			const parsedBody = JSON.parse(res.body);
+			if (parsedBody.error) {
+				this.logger.error({label: 'Slack'}, message, parsedBody);
+			}
+		}
+		catch (err) {
+			this.logger.error({label: 'Slack'}, message, err);
+		}
+	}
+
+	/**
+	 * Send slack message to one of smartprix's channels
+	 * @param {string} text
+	 * @param {object} [param1={}]
+	 * @param {string} [param1.channel] Default value is taken from config
+	 * @param {attach[]} [param1.attachments=[]] Attachments to send
+	 * @param {object} [param1.extraProps={}]
+	 */
+	static async postMessage(text, {
+		channel = getSlackConf().channel,
+		attachments = [],
+		extraProps = {},
+	} = {}) {
+		attachments.push(getDefaultAttachment());
+		const message = Object.assign(extraProps, {
+			text,
+			channel,
+			username: this.username,
+			attachments: JSON.stringify(attachments),
+		});
+
+		if (this.logCondition()) {
+			this.logger.info({label: 'Slack', ...message}, 'Slack message');
+			return;
+		}
+
+		if (getSlackConf().webhook) return this._postWithWebhook(message);
+		return this._postWithToken(message);
+	}
+
+	/**
+	 * Format function for message building, default formatting is Bold
+	 * @param {string} txt
+	 * @param {object} [param1={bold: true}] If no options object is provided then bold is set as true
+	 * @param {boolean} [param1.bold=false]
+	 * @param {boolean} [param1.code=false]
+	 * @param {boolean} [param1.italics=false]
+	 * @param {boolean} [param1.strikethrough=false]
+	 * @returns {string} formatted txt
+	 */
+	static format(txt, {
+		code = false,
+		bold = false,
+		italics = false,
+		strikethrough = false,
+	} = {bold: true}
+	) {
+		if (code) txt = `\`${txt}\``;
+		if (bold) txt = `*${txt}*`;
+		if (italics) txt = `_${txt}_`;
+		if (strikethrough) txt = `~${txt}~`;
+		return txt;
+	}
+
+	/**
+	 * @param {string} url 
+	 * @param {string} text 
+	 */
+	static formatUrl(url, text) {
+		return `<${url}|${text}>`;
+	}
+
+	/**
+	 * @param {string} webhookUrl 
+	 */
+	static setWebhook(webhookUrl) {
+		cfg.set('slack.webhook', webhookUrl);
+	}
+
+	static setToken(token) {
+		cfg.set('slack.token', token);
+	}
+}
+
+module.exports = Slack;
