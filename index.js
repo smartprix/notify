59055f909c281c9bb2a0d551c25ecae6ebae8fb8
Ajeet Goyal <ajeet.goyal@smartprix.com>
2019-08-05 19:33:15 +0530

    read from config for default export

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const Slack = require('./slack');
 const Teams = require('./teams');
+const cfg = require('sm-utils').cfg;
+
+function selectDefault() {
+	switch (cfg('notify', '')) {
+		case 'teams': return Teams;
+		default: return Slack;
+	}
+}
 
 module.exports = {
 	__esModule: true,
-	default: Slack,
+	default: selectDefault(),
 	Slack,
 	Teams,
 };
